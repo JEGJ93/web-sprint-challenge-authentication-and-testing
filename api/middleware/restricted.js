03db7361cc76fa8c61b87f48811a7dc4356350ba
Julio Gomez <jgod93@Julios-MacBook-Pro-20.local>
2021-02-23 14:20:35 -0500

    MvP

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,25 @@
+const jwt = require('jsonwebtoken')
+const { jwtSecret } = require("../auth/config/secrets")
+
+
 module.exports = (req, res, next) => {
-  next();
+
+  const { authorization } = req.headers
+
+  if (authorization) {
+    jwt.verify(authorization, jwtSecret, (err, decodedToken) => {
+      if(err){
+        res.status(401).json({message: "Invalid Credentials"})
+      }
+      else{
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
+  } else{
+    res.status(400).json({ message: "No credentials provided!!"})
+}
+  
   /*
     IMPLEMENT
 
